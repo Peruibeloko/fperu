9765dd40e67d47198c87920f923f4a89894cad1a
Peruibeloko <cmendonca32@gmail.com>
2022-01-13 19:52:54 -0300

    More work

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -8,6 +8,7 @@ import Html.Styled.Events exposing (onClick)
 import Styles exposing (..)
 
 
+main : Program () Model Msg
 main =
     Browser.sandbox { init = init, update = update, view = view >> toUnstyled }
 
@@ -38,35 +39,52 @@ update msg model =
             { model | isSidebarOpen = True }
 
 
+sidebar : Model -> Html Msg
+sidebar model =
+    aside [ id "socials", css [ isBackdropVisible model.isSidebarOpen ] ]
+        [ h1 [] [ text "Redes" ]
+        , span [ id "close", onClick CloseSidebar ] [ text "→" ]
+        , ul []
+            [ a [ href "https://soundcloud.com/dynmic", target "_blank" ]
+                [ li [ id "soundcloud" ] [ text "Soundcloud" ] ]
+            , a [ href "https://www.instagram.com/dyn.mic", target "_blank" ]
+                [ li [ id "instagram" ] [ text "Instagram" ] ]
+            , a [ href "https://www.fb.com/dyn.mic.dnb", target "_blank" ]
+                [ li [ id "facebook" ] [ text "Facebook" ] ]
+            , a [ href "https://www.twitch.tv/dynmicdnb", target "_blank" ]
+                [ li [ id "twitch" ] [ text "Twitch" ] ]
+            , a [ href "https://twitter.com/dynmicdnb", target "_blank" ]
+                [ li [ id "twitter" ] [ text "Twitter" ] ]
+            ]
+        ]
+
+
+customHeader : Html Msg
+customHeader =
+    header [ css headerStyles ]
+        [ h1 [ css headerTitle1 ]
+            [ a [ title "ibe", href "/" ]
+                [ text "Peru" ]
+            ]
+        , h2 [ css headerTitle2 ]
+            [ text "Não tenho muita certeza\nsobre o que é tudo isso aqui" ]
+        , button [ onClick OpenSidebar, css hambButton ]
+            [ text "≡" ]
+        ]
+
+
+fontUrl : String
+fontUrl =
+    "https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500&family=Noto+Serif&family=PT+Sans:ital@0;1&display=swap"
+
+
 view : Model -> Html Msg
 view model =
-    div [ css gridContainer ]
+    node "main"
+        [ css gridContainer ]
         [ globalStyles
-        , span [ id "backdrop", css [ sidebarState model.isSidebarOpen ] ] []
-        , header [ css [ Css.property "grid-area" "header" ] ]
-            [ h1 []
-                [ a [ title "ibe", href "/" ]
-                    [ text "Peru" ]
-                ]
-            , h2 []
-                [ text "Não tenho muita certeza\nsobre o que é tudo isso aqui" ]
-            , button [ id "hamb", onClick OpenSidebar ]
-                [ text "≡" ]
-            ]
-        , aside [ id "socials", css [ sidebarState model.isSidebarOpen ] ]
-            [ h1 [] [ text "Redes" ]
-            , span [ id "close", onClick CloseSidebar ] [ text "→" ]
-            , ul []
-                [ a [ href "https://soundcloud.com/dynmic", target "_blank" ]
-                    [ li [ id "soundcloud" ] [ text "Soundcloud" ] ]
-                , a [ href "https://www.instagram.com/dyn.mic", target "_blank" ]
-                    [ li [ id "instagram" ] [ text "Instagram" ] ]
-                , a [ href "https://www.fb.com/dyn.mic.dnb", target "_blank" ]
-                    [ li [ id "facebook" ] [ text "Facebook" ] ]
-                , a [ href "https://www.twitch.tv/dynmicdnb", target "_blank" ]
-                    [ li [ id "twitch" ] [ text "Twitch" ] ]
-                , a [ href "https://twitter.com/dynmicdnb", target "_blank" ]
-                    [ li [ id "twitter" ] [ text "Twitter" ] ]
-                ]
-            ]
+        , node "link" [ href fontUrl, rel "stylesheet" ] []
+        , span [ id "backdrop", css [ isBackdropVisible model.isSidebarOpen ] ] []
+        , customHeader
+        , sidebar model
         ]
--- client/src/Styles.elm
+++ client/src/Styles.elm
@@ -1,21 +1,89 @@
 module Styles exposing (..)
 
 import Css exposing (..)
-import Css.Global exposing (class, global, selector)
+import Css.Global exposing (global, selector)
 import Html.Styled exposing (Html)
 
 
-commonTheme =
-    { titleSansRegular = fontFamilies [ "Fira Sans", "sans-serif" ]
-    , sansRegular = fontFamilies [ "PT Sans", "sans-serif" ]
-    , serifRegular = fontFamilies [ "Noto Serif", "serif" ]
-    , mainColor = rgb 151 39 39
-    , black = rgb 24 26 27
-    , gray = rgb 93 93 93
-    , darkGray = rgb 20 20 20
-    , lisghtGray = rgb 160 160 160
-    , white = rgb 218 218 218
+type alias Theme =
+    { fonts :
+        { titleSansRegular : List String
+        , sansRegular : List String
+        , serifRegular : List String
+        }
+    , colors :
+        { main : Color
+        , black : Color
+        , gray : Color
+        , darkGray : Color
+        , lisghtGray : Color
+        , white : Color
+        }
     }
+
+
+theme : Theme
+theme =
+    { fonts =
+        { titleSansRegular = [ "Fira Sans", .value sansSerif ]
+        , sansRegular = [ "PT Sans", .value sansSerif ]
+        , serifRegular = [ "Noto Serif", .value serif ]
+        }
+    , colors =
+        { main = rgb 151 39 39
+        , black = rgb 24 26 27
+        , gray = rgb 93 93 93
+        , darkGray = rgb 20 20 20
+        , lisghtGray = rgb 160 160 160
+        , white = rgb 218 218 218
+        }
+    }
+
+
+globalStyles : Html msg
+globalStyles =
+    global
+        [ selector "*"
+            [ margin zero
+            , padding zero
+            , textDecoration none
+            , boxSizing borderBox
+            , listStyle none
+            , backgroundColor theme.colors.black
+            , color theme.colors.white
+            ]
+        ]
+
+
+headerStyles : List Style
+headerStyles =
+    [ property "grid-area" "header"
+    ]
+
+
+headerTitle1 : List Style
+headerTitle1 =
+    [ fontSize (rem 4)
+    , fontFamilies theme.fonts.titleSansRegular
+    , borderLeft3 (px 10) solid theme.colors.main
+    , padding (rem 0.5)
+    ]
+
+
+headerTitle2 : List Style
+headerTitle2 =
+    [ color theme.colors.gray
+    , fontSize (rem 1)
+    , fontFamilies theme.fonts.sansRegular
+    , margin2 (rem 1) zero
+    , whiteSpace pre
+    ]
+
+
+hambButton : List Style
+hambButton =
+    [ display none
+    ]
 
 
 gridContainer : List Style
@@ -28,33 +96,8 @@ gridContainer =
     ]
 
 
-headerStyles : List Style
-headerStyles =
-    [ margin (rem 3)
-    , property "display" "grid"
-    , property "grid-template-areas" """
-        'header post navlist'
-        'socials post navlist'"""
-    ]
-
-
-globalStyles : Html msg
-globalStyles =
-    global
-        [ selector "*"
-            [ margin zero
-            , padding zero
-            , textDecoration none
-            , boxSizing borderBox
-            , listStyle none
-            , backgroundColor commonTheme.black
-            , color commonTheme.white
-            ]
-        ]
-
-
-sidebarState : Bool -> Style
-sidebarState isOpen =
+isBackdropVisible : Bool -> Style
+isBackdropVisible isOpen =
     if isOpen then
         display block
 
